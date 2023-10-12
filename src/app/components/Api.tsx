d2a6cde8ec93554c19fca1e29be19a6fd88166c1
griphaugland <griphaugland@gmail.com>
2023-10-13 00:47:01 +0200

    Fixing APIKEY reveal, old deleted

--- src/app/components/Api.tsx
+++ src/app/components/Api.tsx
@@ -1,12 +1,10 @@
-/* import api_key from "../../../next-env"; */
-
 
 export default async function getProducts(input: string) {
     const res = await fetch(`https://kassal.app/api/v1/products?search=${input}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-            Authorization: `Bearer kQFCAYLDdWl3tr9RhtuGts3VBAaj0TcMoaZZABb1`,
+            Authorization: `Bearer ${process.env.API_KEY}`,
         },
     });
     
